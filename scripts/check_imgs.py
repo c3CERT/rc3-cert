1dd8e4ca958a2f964eea41bdc558609b46d421e5
yourcoke <deinkoks@gmail.com>
2020-11-18 19:46:03 +0100

    added: img dimension check script

---
+++ scripts/check_imgs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from PIL import Image
+import sys
+import os
+
+for root, dirs, files in os.walk('imgs'):
+    for file in files:
+     if file.endswith('.png'):
+      x,y = Image.open(os.path.join(root, file)).size
+      if not(x % 32 == y % 32 == 0):
+          sys.exit("%s has wrong dimensions." % file)
+
