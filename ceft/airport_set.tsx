c6bf5c5f318a5afef9f55f74ecdadce21f3e193d
maqor <quantum@cert.ccc.de>
2021-12-21 13:42:47 +0100

    Del: Alte Mpas, mit Ausnahme CERT Leipzig. Edit: Disclaimer

--- ceft/airport_set.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="airport_set" tilewidth="32" tileheight="32" tilecount="12" columns="4">
- <image source="../airport_set.png" width="128" height="96"/>
-</tileset>
